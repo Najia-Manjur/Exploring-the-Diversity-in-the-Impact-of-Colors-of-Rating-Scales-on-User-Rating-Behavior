d7bc1b078ef33a1f7de9c0696a171fef04bb83b9
najia <najiamanjur@gmail.com>
2021-03-11 23:53:37 -0600

    Commit on Dec 9

---
+++ secrets.sh
@@ -0,0 +1,4 @@
+# secrets
+export SHARED_FOLDER=1HBz1Px27hguzbMw2r3P4SDBVRZOExUdY
+export CLIENT_EMAIL=drive-account@movie-rating-survey.iam.gserviceaccount.com
+export PRIVATE_KEY=$(jq --raw-output .private_key keys.json)
